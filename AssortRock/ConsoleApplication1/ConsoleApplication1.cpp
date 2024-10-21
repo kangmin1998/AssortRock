5b4bd3879b6341355d32aff590f7b35e3348ec18
태민 강 <fkdltbtxj1@gmail.com>
2024-10-21 18:22:33 +0900

    24_10_21 winapi 업데이트

---
+++ AssortRock/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	//  00000000 00000000 00000000 00000001   4바이트
+	int value = 1;
+
+	//	Arr[0]	 Arr[1]	  Arr[2]   Arr[3]
+	//	100		 101	  102	   103
+	//  00000000 00000000 00000000 00000001   4바이트
+	char Arr[4] = {0};
+	int* TestValuePtr = reinterpret_cast<int*>(Arr);
+	*TestValuePtr = value;
+
+	// 위의 내용에
+	// 인텔CPU를 가진 컴퓨터에서 만든 데이터를 AMD CPU에 보내면 값이 이상해진다.
+	// CPU가 빅에디안 리틀에디안을 쓰는지를 알아야 할 떄가 있다.
+
+}
+
